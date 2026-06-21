2aa73934da9a6a9f7160bf1b16e01b738bbcebe3
fabriciodsul <contato.vicemcpe@gmail.com>
2026-06-21 12:43:24 -0300

    chore: gitignore build artifacts em apps/web

--- apps/web/out/_next/static/chunks/app/layout-f9e511a57d691f24.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3871:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8501,23))},8501:()=>{}},e=>{e.O(0,[206,976,785,358],()=>e(e.s=3871)),_N_E=e.O()}]);
